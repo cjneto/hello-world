935367adee563e782aea9583ce6d15ee3cf381f4
Cesar J Neto <cjneto@yahoo.com>
2020-05-31 17:47:53 -0300

    First Sanic Deployment

--- hw.py
+++ hw.py
@@ -1,1 +1,11 @@
-print("Hello World!!")
+from sanic import Sanic
+from sanic.response import json
+
+app = Sanic("hello_example")
+
+@app.route("/")
+async def test(request):
+  return json({"hello": "world"})
+
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", port=8000)
