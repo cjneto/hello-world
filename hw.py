89c06d68b043973f56f5ee39dda16ae254511f92
Cesar J Neto <cjneto@yahoo.com>
2020-06-04 08:48:22 -0300

    just a change for a second build

--- hw.py
+++ hw.py
@@ -5,7 +5,7 @@ app = Sanic("hello_example")
 
 @app.route("/")
 async def test(request):
-  return json({"hello": "world"})
+  return json({"hello": "world", "version": 2})
 
 if __name__ == "__main__":
   app.run(host="0.0.0.0", port=8080)
